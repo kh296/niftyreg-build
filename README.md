ae1ac79b1e51f64eea59e9be544255f96552a03f
K. Harrison <kh296@users.noreply.github.com>
2023-07-11 12:57:29 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# niftyreg-build
-Script for building NiftyReg on Linux and MacOS
+# niftireg-build
+
+[![License](https://img.shields.io/github/license/kh296/niftyreg-build)](https://github.com/kh296/niftyreg-build/blob/master/LICENSE)
+
+This project provides a script for building NiftyReg executables and
+libraries on Linux or MacOS, based on the instructions at:
+
+-  [http://cmictig.cs.ucl.ac.uk/wiki/index.php/NiftyReg_install](http://cmictig.cs.ucl.ac.uk/wiki/index.php/NiftyReg_install)
+
+The script obtains the source code by cloning:
+
+- [https://github.com/KCL-BMEIS/niftyreg](https://github.com/KCL-BMEIS/niftyreg)
+
+Variables that may need to be modified by the user are indicated in the script.
+
+A compressed tarball containing executables and libraries built for
+Linux is linked in the [Releases](https://github.com/kh296/niftyreg-build/releases).  The date (YYYY.MM.DD) in the tarball name indicates the date of cloning
+of the NiftyReg repository used in the build.
---
+++ niftyreg_build.sh
@@ -0,0 +1,73 @@
+# This script builds NiftyReg executables and libraries on Linux or MacOS,
+# based on the instructions at:
+#     http://cmictig.cs.ucl.ac.uk/wiki/index.php/NiftyReg_install
+# It obtains the source code by cloning:
+#     https://github.com/KCL-BMEIS/niftyreg
+#
+# The following variables may need to be modified:
+# CMAKE - path to cmake executable;
+# WORKDIR - path to directory for software build;
+# NIFTYREG - identifier of specific build of NiftyReg.
+#
+# Compiled executables and libraries will be in:
+#     ${WORKDIR}/${NIFTYREG}
+#
+# A compressed tarball of ${WORKDIR}/${NIFTYREG} is created as:
+#     ${WORKDIR}/${NIFTYREG}.tar.gz
+
+#!/bin/bash
+
+# Define path to cmake executable.
+if [[ "$(uname)" == "Darwin" ]]; then
+    CMAKE="/Applications/CMake.app/Contents/bin/cmake"
+else
+    CMAKE="/usr/bin/cmake"
+fi
+
+# Define path to directory for software build, and ensure that it exists.
+WORKDIR="$(pwd)/workdir"
+mkdir -p ${WORKDIR}
+
+# Optionally specify version number, corresponding to numeric part of git tag. 
+# The most recent numeric tag (v1.3.9) is from 3 July 2012,
+# and there have been many commits since.
+# NIFTYREG_VERSION="1.3.9"
+
+# Define paths to directories for Niftyreg source, build, install.
+# Ensure that these directories exist and are empty.
+if [[ -z "${NIFTYREG_VERSION}" ]]; then
+    # No version number specified - identify by build date.
+    NIFTYREG="NiftyReg-$(date +%Y.%m.%d)"
+else
+    # Identify by version number.
+    NIFTYREG="NiftyReg-${NIFTYREG_VERSION}"
+fi
+NIFTYREG_SOURCE="${WORKDIR}/niftyreg"
+NIFTYREG_BUILD="${WORKDIR}/${NIFTYREG}_build"
+NIFTYREG_INSTALL="${WORKDIR}/${NIFTYREG}"
+rm -rf ${NIFTYREG_SOURCE}
+rm -rf ${NIFTYREG_BUILD}
+rm -rf ${NIFTYREG_INSTALL}
+mkdir -p ${NIFTYREG_BUILD}
+mkdir -p ${NIFTYREG_INSTALL}
+
+# Clone the Niftyreg repository.
+cd ${WORKDIR}
+NIFTYREG_URL="https://github.com/KCL-BMEIS/niftyreg"
+git clone ${NIFTYREG_URL}
+# If version number specified, checkout tag.
+if ! [[ -z "${NIFTYREG_VERSION}" ]]; then
+    cd niftyreg
+    git checkout -b v${NIFTYREG_VERSION}_build v${NIFTYREG_VERSION}
+    NIFTYREG_SOURCE=${NIFTYREG_SOURCE}/nifty_reg
+    cd ..
+fi
+
+# Build the software.
+cd ${NIFTYREG_BUILD}
+${CMAKE} -DBUILD_ALL_DEP=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=${NIFTYREG_INSTALL} -DUSE_ALL_COMPONENTS=ON ${NIFTYREG_SOURCE} &> niftyreg_cmake.log
+make &> niftyreg_make.log
+make install &> niftyreg_make_install.log
+
+# Create tarball.
+tar -zcvf ${NIFTYREG_INSTALL}.tar.gz ${NIFTYREG_INSTALL}
